3fafe8bc5d17c18fee912fb7cd09d019e75d49de
Anoop Wilson <anoopwilson33@yahoo.com>
2024-03-07 17:03:38 +0530

    Added 'recruitment' section

--- script.js
+++
@@ -1,5 +0,0 @@
-<span class="material-symbols-outlined">expand_more</span>
-<span class="material-symbols-outlined">expand_more</span>
-<span class="material-symbols-outlined">expand_more</span>
-<span class="material-symbols-outlined">expand_more</span>
-<span class="material-symbols-outlined">expand_more</span>
